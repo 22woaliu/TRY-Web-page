6691e3eecefa7fbe1b6059d0604eb0f5bdf22576
Woaliu Hasan <hasan2207090@stud.kuet.ac.bd>
2026-06-12 23:31:26 +0600

    Added the admin login with a static mail and static password

--- admin_login.php
+++ admin_login.php
@@ -7,22 +7,44 @@
 </head>
 <body>
     <main class="content-container">
-        <?php 
-            $pageTitle = "Home Page"; 
-            include('header.php'); 
+        <br><br>
+        <?php
+            session_start();
+
+            $staticEmail = "admin@try.org";
+            $staticPassword = "password123";
+            $errorMessage = "";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+                $inputEmail = trim($_POST['email']);
+                $inputPassword = $_POST['password'];
+
+                if ($inputEmail === $staticEmail && $inputPassword === $staticPassword) {
+                    header("Location: admin_page.html");
+                    exit();
+                } else {
+                    $errorMessage = "Invalid email or password!";
+                }
+            }
+            $pageTitle = "Admin Login";
+            include('header.php');
         ?>
         <br><br>
-        
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="join-form">
-                
-                <label for="email" style="font-size: 20px;"><b>Email:</b></label><br>
-                <input type="email" id="email" name="email" placeholder="Enter your email"><br><br>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="join-form">
+            <?php if (!empty($errorMessage)): ?>
+                <div style="color: red; font-weight: bold; margin-bottom: 15px; text-align: center;">
+                    <?php echo $errorMessage; ?>
+                </div>
+            <?php endif; ?>
 
-                <label for="password" style="font-size: 20px;"><b>Password:</b></label><br>
-                <input type="password" id="password" name="password" placeholder="Enter your password"><br><br>
+            <label for="email" style="font-size: 20px;"><b>Email:</b></label><br>
+            <input type="email" id="email" name="email" placeholder="Enter your email" required><br><br>
 
-                <input type="submit" name="submit" value="Submit">
-            </form>
+            <label for="password" style="font-size: 20px;"><b>Password:</b></label><br>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required><br><br>
+
+            <input type="submit" name="submit" value="Submit">
+        </form>
         </div><br><br>
 
         <?php 
